c59327c3e2fd601447e53cb83a72401ce85ff81c
AngelicaJH <isc.florjardinez@gmail.com>
2021-08-19 19:28:33 -0500

    se inició la implementación de fire base

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { myFunction, email, password } from './lib/index.js';
 
 myFunction();
+auth
+.createUserWithEmailAndPassword(email, password)
+.then(userCredential=>{
+  register.reset();
+})
