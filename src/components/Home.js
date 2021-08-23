d1828838b231a97c8f59d92ad12e2337ab57b031
Nancy <nancy.090495@gmail.com>
2021-08-23 12:11:36 -0500

    imprimiendo Home en interfaz con template

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,37 @@
-import { onNavigate } from '../main.js';
-import { toViewSingUp } from './singUp.js';
+import { onNavigate } from '../routes.js';
+//import { toViewSingUp } from './singUp.js';
 
-export const toViewHome = () => {
-  const mainDiv = document.createElement('div');
-  mainDiv.textContent = 'Regístrate';
-  const buttonRegister = document.createElement('button');
-  buttonRegister.textContent = 'Regístrate';
+// export const toViewHome = () => {
+//   const mainDiv = document.createElement('div');
+//   mainDiv.textContent = 'Regístrate';
+//   const buttonRegister = document.createElement('button');
+//   buttonRegister.textContent = 'Regístrate';
 
-  mainDiv.appendChild(buttonRegister);
+//   mainDiv.appendChild(buttonRegister);
 
-  return mainDiv;
-};
-
-toViewHome();
-// export const toViewHome = (container) => {
-//   const html = `
-//     <section class="container">
-//         <img class="logo" src="img/PIC&ART.png" alt="logo">
-//         <p>Share your ideas in the largest art community. Get inspired with different styles and genres around your country.
-//         </p>
-//         <input type="button" class="btn_log login" value="LOG IN" />
-//         <input type="button" class="btn_log signup" value="SIGN UP" />
-//         <p2>or</p2>
-//         <input type="button" class="btn_log google" value="Continue with Google" />
-//     </section>`;
-//   container.innerHtml = html;
+//   return mainDiv;
 // };
+
+// toViewHome();
+
+export const toViewHome = (container) => {
+  console.log("estoy en home");
+  const html = `
+    <section class="container">
+        <img class="logo" src="img/PIC&ART.png" alt="logo">
+        <p>Share your ideas in the largest art community. Get inspired with different styles and genres around your country.
+        </p>
+        <input type="button" class="btn_log login" value="LOG IN" />
+        <input type="button" class="btn_log signup" value="SIGN UP" id="toCreate" />
+        <p2>or</p2>
+        <input type="button" class="btn_log google" value="Continue with Google" />
+    </section>`;
+
+  container.innerHTML = html
+  const toCreate = document.getElementById("toCreate") 
+  toCreate.addEventListener('click', (e) => {
+    
+   e.preventDefault();
+   onNavigate('/signUp');
+  });
+};
---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import { toViewHome } from './components/Home.js';
+import { toViewSignUp } from './components/signUp.js';
+
+
+const rootDiv = document.getElementById('root');
+
+export const routes = {
+  '/': toViewHome,
+  '/signUp': toViewSignUp,
+};
+
+//rootDiv.innerHTML = routes[window.location.pathname];
+//rootDiv.innerHTML(component());
+
+export const onNavigate = (pathname) => {
+  
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname
+  );
+  const component = routes[pathname]
+  component(rootDiv);
+}
+
+window.onpopstate = () => {
+  const component = routes[window.location.pathname]
+  component(rootDiv)
+}
