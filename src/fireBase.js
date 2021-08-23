3afaca1c249b968ca056ffa2d76b977b56ea5ccf
ColetteOrdz <pcoletteor@gmail.com>
2021-08-23 10:55:46 -0500

    correcciones

--- src/fireBase.js
+++ src/fireBase.js
@@ -1,24 +1,24 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCaBVEyo0yKvVWGvxHpTufYnUwG7qMZ2FY',
-  authDomain: 'pic-art-1c2c5.firebaseapp.com',
-  projectId: 'pic-art-1c2c5',
-  storageBucket: 'pic-art-1c2c5.appspot.com',
-  messagingSenderId: '866895313531',
-  appId: '1:866895313531:web:c14ccfb8c6766208a540de',
-  measurementId: 'G-V85MSYGCCW',
+    apiKey: 'AIzaSyCaBVEyo0yKvVWGvxHpTufYnUwG7qMZ2FY',
+    authDomain: 'pic-art-1c2c5.firebaseapp.com',
+    projectId: 'pic-art-1c2c5',
+    storageBucket: 'pic-art-1c2c5.appspot.com',
+    messagingSenderId: '866895313531',
+    appId: '1:866895313531:web:c14ccfb8c6766208a540de',
+    measurementId: 'G-V85MSYGCCW',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 const auth = firebase.auth();
 
- const register = (singUpEmail, singUpPassword) => {
-  auth.createUserWithEmailAndPassword(singUpEmail, singUpPassword)
-    .then((userCredential) => {
-      register.reset();
-    });
+const register = (singUpEmail, singUpPassword) => {
+    auth.createUserWithEmailAndPassword(singUpEmail, singUpPassword)
+        .then((userCredential) => {
+            register.reset();
+        });
 };
 // export const register = document.querySelector("#submitForm");
 // register.addEventListener('submit', (e) => {
--- src/main.js
+++ src/main.js
@@ -6,23 +6,22 @@ import { toViewSingUp } from './components/singUp.js';
 const rootDiv = document.getElementById('root');
 
 export const routes = {
-  '/': toViewHome,
-  '/singUp': toViewSingUp,
+    '/': toViewHome,
+    '/singUp': toViewSingUp,
 };
-console.log(routes);
+//console.log(routes);
 
-// const component = routes[window.location.pathname];
+rootDiv.innerHTML(component()); = routes[window.location.pathname];
 // rootDiv.innerHTML(component());
 
 export const onNavigate = (pathname) => {
-  
-  window.history.pushState(
-    {},
-    pathname,
-    window.location.origin + pathname
-  );
-  const component = routes[pathname]
-  component(rootDiv);
+
+    window.history.pushState({},
+        pathname,
+        window.location.origin + pathname
+    );
+    // const component = routes[pathname]
+    // component(rootDiv);
 };
 
 // aqui tu codigo
