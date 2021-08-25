8044b2a695f89121ec232ec04d6e9530dbdfb285
ColetteOrdz <pcoletteor@gmail.com>
2021-08-24 20:03:18 -0500

    estilos código

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,29 @@
-import { onNavigate } from '../routes.js';
+/* eslint-disable */
+
+import { onNavigate } from "../routes.js";
 
 export const toViewHome = (container) => {
     console.log("estoy en home");
     const html = `
-    <section class="container">
-        <img class="logo" src="img/PIC&ART.png" alt="logo">
+       <section class='container'>
+        <img class='logo' src='img/PIC&ART.png' alt='logo'>
         <p>Share your ideas in the largest art community. Get inspired with different styles and genres around your country.
         </p>
-        <input type="button" class="btn_log login" value="LOG IN" id="toLogIn" />
-        <input type="button" class="btn_log signup" value="SIGN UP" id="toCreate" />
+        <input type='button' class='btn_log login' value='LOG IN' id='toLogIn' />
+        <input type='button' class='btn_log signup' value='SIGN UP' id='toCreate' />
         <p2>or</p2>
-        <input type="button" class="btn_log google" value="Continue with Google" />
+        <input type='button' class='btn_log google' value='Continue with Google' />
     </section>`;
 
-    container.innerHTML = html
-    const toCreate = document.getElementById("toCreate")
-    toCreate.addEventListener('click', (e) => {
-
+    container.innerHTML = html;
+    const toCreate = document.getElementById("toCreate");
+    toCreate.addEventListener("click", (e) => {
         e.preventDefault();
-        onNavigate('/signUp');
+        onNavigate("/signUp");
     });
-    const toLogIn = document.getElementById("toLogIn")
-    toLogIn.addEventListener('click', (e) => {
-
+    const toLogIn = document.getElementById("toLogIn");
+    toLogIn.addEventListener("click", (e) => {
         e.preventDefault();
-        onNavigate('/LogIn');
+        onNavigate("/LogIn");
     });
 };
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,33 +1,35 @@
+/* eslint-disable */
+
 import { logIn } from '../lib/fireBase.js'
 export { onNavigate }
 from '../routes.js';
 
 
 export const toViewLogIn = (container) => {
-    console.log("Estoy en LogIn");
-    const html = ` <section class="container logIn-form">
-    <img class="logo" src="img/PIC&ART.png" alt="logo">
-    <form id="submitForm">
-        <br><label value="email">E-mail</label><br>
-        <input id="logInEmail" type="text" placeholder="e-mail" required/><br>
-        <label value="password">Password</label><br>
-        <input id="logInPassword" type="password" placeholder="Password" required />
-        <input src="../img/pngegg.png" id="viewPass" type="image" />
-        <input type="submit" class="btn_log signup" value="LOG IN" /><br>
+    console.log('Estoy en LogIn');
+    const html = ` <section class='container logIn-form'>
+    <img class='logo' src='img/PIC&ART.png' alt='logo'>
+    <form id='logInForm'>
+        <br><label value='email'>E-mail</label><br>
+        <input id='logInEmail' type='text' placeholder='e-mail' required/><br>
+        <label value='password'>Password</label><br>
+        <input id='logInPassword' type='password' placeholder='Password' required />
+        <input src='../img/pngegg.png' id='viewPass' type='image' />
+        <input type='submit' class='btn_log signup' value='LOG IN' /><br>
         <p2>or</p2><br>
-        <input type="button" class="btn_log google" value="Continue with Google" />
+        <input type='button' class='btn_log google' value='Continue with Google' />
         <br>
     </form>
     
-    <a href="javascript:history.back();"> <img class="return" src="img/return.png" alt="logo"   ></a>
+    <a href='javascript:history.back();'> <img class='return' src='img/return.png' alt='logo'   ></a>
 </section>`;
 
     container.innerHTML = html
 
-    document.getElementById("viewPass").addEventListener("click", (e) => {
+    document.getElementById('viewPass').addEventListener('click', (e) => {
         e.preventDefault();
-        const inputType = document.getElementById("logInPassword");
-        inputType.type == "password" ? inputType.type = "text" : inputType.type = "password"
+        const inputType = document.getElementById('logInPassword');
+        inputType.type == 'password' ? inputType.type = 'text' : inputType.type = 'password'
     });
 
 
@@ -37,7 +39,7 @@ export const toViewLogIn = (container) => {
         const password = document.querySelector('#logInPassword').value;
 
         e.preventDefault();
-        console.log('estoy evitando el reset');
+        //console.log('estoy evitando el reset');
         console.log(email, password);
 
         logIn(email, password);
