6157edca63cc16144aa093519c16d74255f7d51b
CarolinaCasas <casasc9@uabc.edu.mx>
2021-08-27 18:04:42 -0700

    addAllRoutes

--- src/components/home.js
+++ src/components/home.js
@@ -1,48 +1,10 @@
-export const home = () => {
-    
-    const homePage = document.createElement("div");
-    homePage.classList.add("homepage");
-    const banner = document.createElement("header");
-    banner.setAttribute("id", "banner");
-    const petFriends = document.createElement("h1");
-    petFriends.setAttribute("id", "petFriends");
-    petFriends.textContent = "PetFriends";
-    const logo = document.createElement ("img")
-    logo.setAttribute("id", "logo");
-    logo.src = "./imagenes/Imagen1.png";
-    const email = document.createElement("input");
-    email.setAttribute("id", "email");
-    email.setAttribute("placeholder", "Correo electrónico");
-    const password = document.createElement("input");
-    password.setAttribute("id", "password");
-    password.setAttribute("placeholder", "Contraseña");
-    const signIn = document.createElement("button");
-    signIn.setAttribute("id", "signIn");
-    signIn.textContent = "Iniciar sesión";
-    const signInGoogle = document.createElement("button");
-    signInGoogle.setAttribute("id", "signInGoogle");
-    signInGoogle.textContent = "Continuar con Google";
-    const registerIn = document.createElement("h3");
-    registerIn.setAttribute("id", "registerIn");
-    registerIn.textContent = "¿No tienes una cuenta?";
-    const registerButton = document.createElement("button");
-    registerButton.setAttribute("Id", "registerButton");
-    registerButton.textContent = "Regístrate";
-    const formContainer =document.createElement("form")
-    formContainer.setAttribute("id", "formContainer");
+import { onNavigate } from "../main.js";
 
-    homePage.appendChild(banner);
-    banner.appendChild(logo);
-    banner.appendChild(petFriends);
-    homePage.appendChild(formContainer);
-    formContainer.appendChild(email);
-    formContainer.appendChild(password);
-    formContainer.appendChild(signIn);
-    formContainer.appendChild(signInGoogle);
-    formContainer.appendChild(registerIn);
-    formContainer.appendChild(registerButton);
-    
-
-
-    return homePage;
+export const home = ()=>{
+    const registerPage = document.createElement('div');
+    const signOut = document.createElement('button');
+    signOut.textContent = 'Cerrar sesión';
+    registerPage.appendChild(signOut);
+    signOut.addEventListener('click', () => onNavigate('/'));
+return registerPage;
 }
