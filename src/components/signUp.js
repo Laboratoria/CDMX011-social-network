cc3420d359c90d74a97dd143f82884c84ce7d7a1
AngelicaJH <isc.florjardinez@gmail.com>
2021-08-31 19:21:57 -0500

    archivo SignUp

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,53 +1,51 @@
-/* eslint-disable */
+/* eslint-disable import/no-cycle */
+import { onNavigate } from '../main.js';
 
-import { onNavigate } from '../routes.js';
-import { register } from '../lib/fireBase.js';
+export const SignUp = () => {
+        const SignUpContainer = document.createElement("div");
+        const buttonSignUp = document.createElement("button");
+        const buttonHome = document.createElement("button");
 
-export const toViewSignUp = (container) => {
-    console.log('Estoy en signUp')
-    const html = ` <section class='container signup-form'>
-    <img class='logo' src='img/PIC&ART.png' alt='logo'>
-    <form id='submitForm'>
-        <br><label value='email'>E-mail*</label>
-        <input id='singUpEmail' type='text' placeholder='e-mail' required/><br>
-        <br><label value='password'>Password*</label>
-        <input id='singUpPassword' type='password' placeholder='Password' required/>
-        <input id='viewPass' src='../img/eye.png' type='image'/>
-        <br><input type='submit' class='btn_log signup' value='SIGN UP' /><br>
-        <p class="error"></p>
-      
-        
-       
-    </form>
-    <a href='javascript:history.back();'> <img class='return' src='img/return-logo.png' alt='logo' ></a>
-</section>`;
-    container.innerHTML = html
+        SignUpContainer.textContent = "Crea tu cuenta";
+        buttonHome.textContent = "Regresar";
+        buttonSignUp.textContent = "Regístrate";
 
-    document.getElementById('viewPass').addEventListener('click', (e) => {
-        e.preventDefault();
-        const inputType = document.getElementById('singUpPassword');
-        inputType.type == 'password' ? inputType.type = 'text' : inputType.type = 'password';
-    });
+        buttonHome.addEventListener("click", () => onNavigate("/"));
 
-    const singUpform = document.querySelector('#submitForm');
-    singUpform.addEventListener('submit', (e) => {
-        const email = document.querySelector('#singUpEmail').value;
-        const password = document.querySelector('#singUpPassword').value;
+        SignUpContainer.appendChild(buttonHome);
+        SignUpContainer.appendChild(buttonSignUp);
 
-        e.preventDefault();
-        e.stopPropagation();
-        console.log('estoy evitando el reset');
-        console.log(email, password);
+        return SignUpContainer;
+    }
+    // import { onNavigate } from '../main.js';
 
-        register(email, password)
-        .then(() => {
-          console.log('then');
-            onNavigate('/TimeLine');
-        })
-            .catch((error) => {
-                const alertaError = error.message;
-                document.querySelector('.error').innerHTML = `${alertaError}`;
-                console.log(error.message);
-            });
-    });
-};
+// export const toViewSingUp = (container) => {
+//   const html = ` <section class="container signup-form">
+//     <img class="logo" src="img/PIC&ART.png" alt="logo">
+//     <form id="submitForm">
+//         <label value="username">User name</label><br>
+//         <input  type="text" placeholder="User name" /><br>
+//         <label value="email">E-mail</label><br>
+//         <input id="singUpEmail" type="text" placeholder="e-mail" /><br>
+//         <label value="password">Password</label><br>
+//         <input id="singUpPassword" type="password" placeholder="Password" /><br>
+//         <input type="submit" class="btn_log signup" value="SIGN UP" /><br>
+//         <p2>or </p2><br>
+//         <input type="button" class="btn_log google" value="Continue with Google" />
+//     </form>
+// </section>`;
+//   container.innerHtml = html;
+//   const singUpform = document.querySelector('#submitForm');
+//   singUpform.addEventListener('submit', (e) => {
+//     const email = document.querySelector('#singUpEmail').value;
+//     const password = document.querySelector('#singUpPassword').value;
+
+//     e.preventDefault();
+//     console.log('estoy evitando el reset');
+//     console.log(email + password);
+
+//     register(email, password);
+
+//     singUpform.reset();
+//   });
+// };
