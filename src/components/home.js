99fe09e7d36e262a3a8e5b0fb998da380eefafee
berenicehr <bere4758@gmail.com>
2021-09-06 15:59:57 -0500

    Co-authored-by: karen-gup <karen-gup@users.noreply.github.com>

--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import { onNavigate } from '../main.js';
 
 export const home = () => {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,19 @@
 import { register } from '../src/components/register.js';
-
+/*
 describe('DOM behavior', () => {
   test('it should bring DOM elements', () => {
     const registerTest = register();
     expect(registerTest).toBe('HTMLDivElement');
+  });
+});
+*/
+
+describe('register', () => {
+  document.body.innerHTML = `<div id = "root"></div>`;
+  it('should render string template from JS', () => {
+    const component = register();
+    render(container, component);
+
+    console
   });
 });
