a3291f3615033a0fe219c54b92fc2e845f1252ae
ColetteOrdz <pcoletteor@gmail.com>
2021-09-07 18:28:44 -0500

    test Home

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -4,19 +4,7 @@
 /* eslint-disable */
 
 import './globals/firebase.js';
-import { toViewHome } from '../src/components/Home.js';
 import { toViewLogIn} from '../src/components/LogIn.js';
-
-
-// describe("Colección de test en Home", () => {
-//     document.body.innerHTML=`<div id="root"></div>`;
-    
-//     it("Should render in Home", () => {
-//         const rootDiv=document.getElementById('root');
-//         console.log(rootDiv);
-//         expect(typeof toViewHome).toBe('function');
-//     });
-// });
 
 describe("Colección de test en logIn", () => {
         document.body.innerHTML=`<div id="root"></div>`;
