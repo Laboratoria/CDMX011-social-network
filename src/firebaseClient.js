b4c3f3018387d5b5ac102331b61e8949e6698de6
ezritz <lidia.edith.escoto@gmail.com>
2021-09-07 22:24:45 -0500

    Change firebase config

--- src/firebaseClient.js
+++ src/firebaseClient.js
@@ -1,11 +1,4 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyB1pNKBmF7G0ZRIEtlByBlsS_Vrrd_5VAc',
-  authDomain: 'red-social-5aebc.firebaseapp.com',
-  projectId: 'red-social-5aebc',
-  storageBucket: 'red-social-5aebc.appspot.com',
-  messagingSenderId: '345107510223',
-  appId: '1:345107510223:web:e9601e9b1171d8449c58ba',
-};
+import { firebaseConfig } from './firebase.config.js'
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
