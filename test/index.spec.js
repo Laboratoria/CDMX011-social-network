9a189c062a387b404706de0ebd92122df31678e5
berenicehr <bere4758@gmail.com>
2021-09-08 19:59:56 -0500

    enter

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,19 +1,17 @@
-import { register } from '../src/components/register.js';
-/*
+/* import { register } from '../src/components/register.js';
+
 describe('DOM behavior', () => {
   test('it should bring DOM elements', () => {
     const registerTest = register();
     expect(registerTest).toBe('function');
   });
 });
-*/
 
 describe('register', () => {
-  document.body.innerHTML = `<div id = "root"></div>`;
+  document.body.innerHTML = '<div id = "root"></div>';
   it('should render string template from JS', () => {
     const component = register();
     render(container, component);
-
-    console
   });
 });
+*/
