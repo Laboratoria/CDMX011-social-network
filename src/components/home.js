509d74e480e9b865973f4bf5474e3bf205256cf3
berenicehr <bere4758@gmail.com>
2021-09-21 19:59:09 -0500

    userEmailOnPost

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,6 @@
 import { onNavigate } from '../main.js';
 import {
-  logOut, getUser, postInFirestore, updatePost,
+  logOut, getUser, postInFirestore, updatePost, db,
 } from '../firebaseAuth.js';
 import { allFunctions } from '../lib/validFunc.js';
 
@@ -8,10 +8,10 @@ export const home = () => {
   let userEmail = getUser();
   if (userEmail !== null) {
     userEmail = userEmail.email;
-
-    const homePage = document.createElement('div');
-    homePage.setAttribute('id', 'homePage');
-    const htmlNodes = `<header id = "wallBanner" >
+  }
+  const homePage = document.createElement('div');
+  homePage.setAttribute('id', 'homePage');
+  const htmlNodes = `<header id = "wallBanner" >
   <img id="logoWall" src="./imagenes/Imagen1.png">
   <h1 id="petFriendsWall">Pet Friends</h1>
   <img id="signOut" src= "./imagenes/exit.png"></header>
@@ -30,15 +30,14 @@ export const home = () => {
   </div>
   <div id="posts"></div>
   `;
-    homePage.innerHTML = htmlNodes;
+  homePage.innerHTML = htmlNodes;
 
-    // printPostFromFirestore()
-    // .then((snapshot) => {
-    updatePost((snapshot) => {
-      const postDivPublish = homePage.querySelector('#posts');
-      postDivPublish.innerHTML = '';
-      snapshot.forEach((doc) => {
-        const htmlPostsPublished = `<div id= "recentPostDiv">
+  const postDivPublish = homePage.querySelector('#posts');
+
+  updatePost((snapshot) => {
+    postDivPublish.innerHTML = '';
+    snapshot.forEach((doc) => {
+      const htmlPostsPublished = `<div id= "recentPostDiv">
           <p id="userMail">${doc.data().user}</p>
           <p id="recentPost">${doc.data().post}</p>
           <div id= "divButtons"><button id= "edit">Editar</button>
@@ -47,38 +46,44 @@ export const home = () => {
           </div>
           </div>`;
 
-        postDivPublish.innerHTML += htmlPostsPublished;
+      postDivPublish.innerHTML += htmlPostsPublished;
+      /* postDivPublish.querySelector('#deletes').addEventListener('click', (e) => {
+        e.stopPropagation();
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collections('posts').doc(id).delete();
+      }); */
+      postDivPublish.querySelector('#deletes').addEventListener('click', (e) => {
+        e.stopPropagation();
+        const id = e.target.parentElement.getAttribute('data-id');
+        db.collections('posts').doc(id).delete();
+        console.log('hola');
       });
     });
+  });
 
-    const modal = homePage.querySelector('#backModal');
-    homePage.querySelector('#signOut').addEventListener('click', () => logOut(onNavigate));
+  const modal = homePage.querySelector('#backModal');
+  homePage.querySelector('#signOut').addEventListener('click', () => logOut(onNavigate));
 
-    homePage.querySelector('#postInput').addEventListener('click', () => {
-      modal.style.visibility = 'visible';
-      homePage.querySelector('#post').value = '';
-    });
+  homePage.querySelector('#postInput').addEventListener('click', () => {
+    modal.style.visibility = 'visible';
+    homePage.querySelector('#post').value = '';
+  });
 
-    homePage.querySelector('#close').addEventListener('click', () => {
-      modal.style.visibility = 'hidden';
-    });
+  homePage.querySelector('#close').addEventListener('click', () => {
+    modal.style.visibility = 'hidden';
+  });
 
-    homePage.querySelector('#share').addEventListener('click', () => {
-      modal.style.visibility = 'hidden';
-      // const catchPost = homePage.querySelector('#catchPost');
-      const postPublish = homePage.querySelector('#post').value;
-      if (allFunctions.validPost(postPublish) === false) {
-        alert('No has publicado un post aún');
-      } else {
-        postInFirestore(postPublish, userEmail);
-      }
+  homePage.querySelector('#share').addEventListener('click', () => {
+    modal.style.visibility = 'hidden';
+    // const catchPost = homePage.querySelector('#catchPost');
+    const postPublish = homePage.querySelector('#post').value;
+    if (allFunctions.validPost(postPublish) === false) {
+      alert('No has publicado un post aún');
+    } else {
+      postInFirestore(postPublish, userEmail);
+    }
 
     // const catchPost = homePage.querySelector('#catchPost');
-    });
-    return homePage;
-    /* console.log(persistance(userEmail)); */
-  }
-
-  // eslint-disable-next-line no-alert
-  return alert('inicia sesión');
+  });
+  return homePage;
 };
