169b343c605e52f022bfdc82761e99dbe7732b7c
YudexiOrozco <86724658+YudexiOrozco@users.noreply.github.com>
2021-09-26 23:47:43 -0500

    Estructura y rutas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,54 @@
-// Este es el punto de entrada de tu aplicacion
+import { Login } from './components/Login.js';
+import { Register } from './components/Register.js';
+import { Wall } from './components/Wall.js';
 
-import { myFunction } from './lib/index.js';
+const rootDiv = document.getElementById('root');
 
-myFunction();
+const routes = {
+  '/': Login,
+  '/register': Register,
+  '/wall': Wall,
+};
+
+export const onNavigate = (pathname) => {
+  // Cambia la ruta de la barra de direcciones actual
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+  // Actualiza el contenido de la página
+  while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.lastChild);
+  }
+
+  rootDiv.appendChild(routes[pathname]());
+  // routes['/wall']  >  Wall()
+};
+
+const component = routes[window.location.pathname];
+
+window.addEventListener('popstate', () => {
+  // Actualiza el contenido de la página
+  while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.lastChild);
+  }
+  rootDiv.appendChild(component());
+});
+
+// window.onload = () => {
+//   rootDiv.appendChild(component());
+// };
+
+// Mantener la sesion activa
+firebase.auth().onAuthStateChanged((user) => {
+  let pathname = window.location.pathname;
+  // Verifica si usuario esta logueado
+  if (user) {
+    pathname = '/wall';
+  } else {
+    pathname = '/';
+  }
+  // Para actualizar la pagina
+  onNavigate(pathname);
+});
