ab33dd36070fd63dbcc4b6ca8dc8689f43814b2f
Yenny Maldonado <yenyamaldonado@gmail.com>
2021-11-16 13:42:17 -0600

    cambia formato de fecha y muestra icono de like

--- src/Firestoredb.js
+++ src/Firestoredb.js
@@ -1,24 +1,21 @@
 import { CardPost } from './components/CardPost.js';
 
 const db = firebase.firestore();
+
 export const userData = (message) => {
-  console.log('entra userData');
   const users = {
     // avatarPlaceholder: './components/avatar.png',
     // name: 'Yenny Maldonado',
     messagePost: message,
     date: new Date(),
-    // likes: './components/thumbs-up.png',
-    // likesCounter: 0,
+    likes: './components/thumbs-up.png',
+    likesCounter: 0,
   };
-
-  // const insertPost = document.querySelector('.newPostDiv');
-  // const divFeed = document.querySelector('.feedDiv');
 
   db.collection('usersApp')
     .add(users)
     .then((docRef) => {
-      console.log(users, docRef);
+      console.log(users, docRef, 'línea 19');
       return users;
     })
     .catch((error) => {
--- src/components/CardPost.js
+++ src/components/CardPost.js
@@ -1,16 +1,25 @@
 export const CardPost = (postData) => {
   const newPostDiv = document.createElement('div');
-  newPostDiv.className = 'newPostDiv';
-
+  const imgAvatar = document.createElement('img');
   const textPost = document.createElement('div');
-  textPost.className = 'textPost';
-  textPost.textContent = postData.messagePost;
-  newPostDiv.appendChild(textPost);
-
   const datePost = document.createElement('div');
-  datePost.class = 'datePost';
-  datePost.textContent = postData.date;
+  const imgLikes = document.createElement('img');
+
+  const formatDate = new Date(postData.date.toDate());
+
+  newPostDiv.className = 'newPostDiv';
+  imgAvatar.className = 'imgAvatar';
+  textPost.className = 'textPost';
+  datePost.className = 'datePost';
+  imgLikes.className = 'imgLikes';
+
+  imgLikes.src = './components/thumbs-up.png';
+
+  textPost.textContent = postData.messagePost;
+  datePost.textContent = formatDate.toLocaleString('en-GB');
+  newPostDiv.appendChild(textPost);
   newPostDiv.appendChild(datePost);
+  newPostDiv.appendChild(imgLikes);
 
   return newPostDiv;
 };
